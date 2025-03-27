8d42076092b136621ef9b833ee0e97aa55f3e426
Sayumi <156690349+SayumiNelithma@users.noreply.github.com>
2025-03-27 22:44:03 +0530

    commit

--- CaseInitiation/src/server/index.js
+++ CaseInitiation/src/server/index.js
@@ -24,7 +24,7 @@ const allowedOrigins = ["http://localhost:5173"]; // Use HTTP instead of HTTPS f
 app.use(
   cors({
     origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT","PATCH", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow cookies
   })
